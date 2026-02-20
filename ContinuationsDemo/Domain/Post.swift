47f3642860cc113ddfb548a92e587777c11475d2
AbrahamGlzP <abrahamglzp@gmail.com>
2026-02-19 18:53:32 -0600

    Created DTO and Domain Model

---
+++ ContinuationsDemo/Domain/Post.swift
@@ -0,0 +1,12 @@
+//
+//  Post.swift
+//  ContinuationsDemo
+//
+//  Created by Abraham Gonzalez Puga on 19/02/26.
+//
+
+struct Post {
+    let id: Int
+    let title: String
+    let body: String
+}
